580da4125b2d9bb215a40c4148f2d09cb8990f32
Sam Christiansen <boonezar@yahoo.com>
2022-12-30 15:43:03 -0700

    clean up click listeners and some string resources

--- app/src/main/java/com/example/codelabpractices/MainActivity.kt
+++ app/src/main/java/com/example/codelabpractices/MainActivity.kt
@@ -3,42 +3,18 @@ package com.example.codelabpractices
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun launchOneDotTwo(view: View) {
-        val intent = Intent(this, CodeLab1Dot2::class.java)
-        startActivity(intent)
+        findViewById<TextView>(R.id.codeLab1Dot2).setOnClickListener { startActivity(Intent(this, CodeLab1Dot2::class.java)) }
+        findViewById<TextView>(R.id.codeLab1Dot3).setOnClickListener { startActivity(Intent(this, CodeLab1Dot3::class.java)) }
+        findViewById<TextView>(R.id.codeLab2Dot1).setOnClickListener { startActivity(Intent(this, CodeLab2Dot1::class.java)) }
+        findViewById<TextView>(R.id.codeLab2Dot3).setOnClickListener { startActivity(Intent(this, CodeLab2Dot3::class.java)) }
+        findViewById<TextView>(R.id.codeLab3Dot1).setOnClickListener { startActivity(Intent(this, CodeLab3Dot1::class.java)) }
+        findViewById<TextView>(R.id.composeList).setOnClickListener { startActivity(Intent(this, ComposeDynamicList::class.java)) }
     }
-
-    fun launchOneDotThree(view: View) {
-        val intent = Intent(this, CodeLab1Dot3::class.java)
-        startActivity(intent)
-    }
-
-    fun launchTwoDotOne(view: View) {
-        val intent = Intent(this, CodeLab2Dot1::class.java)
-        startActivity(intent)
-    }
-
-    fun launchTwoDotThree(view: View) {
-        val intent = Intent(this, CodeLab2Dot3::class.java)
-        startActivity(intent)
-    }
-
-    fun launchComposeList(view: View) {
-        val intent = Intent(this, ComposeDynamicList::class.java)
-        startActivity(intent)
-    }
-
-    fun launchThreeDotOne(view: View) {
-        val intent = Intent(this, CodeLab3Dot1::class.java)
-        startActivity(intent)
-    }
-
 }
